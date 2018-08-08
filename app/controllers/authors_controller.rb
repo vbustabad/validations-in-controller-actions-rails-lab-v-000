cd03cf1f6896f2aea9e649ad448bd7999e1621d1
Veronica Bustabad <vbustabad@gmail.com>
2018-08-08 02:56:11 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,7 +9,7 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.new(author_params)
 
-    if @author.valid?
+    if @author
       @author.save
       redirect_to author_path(@author)
     else
